eca03af539a38f633ef7c89a2267897bb2ec00b3
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-08-07 06:01:13 +0500

    set the navbar dynamically on each page

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,12 @@
 import MainNavigation from './MainNavigation';
-import classes from './Layout.module.css';
+import styles from './Layout.module.css';
 
-function Layout(props) {
+function Layout({ children }) {
+  const { main } = styles;
   return (
     <div>
       <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
+      <main className={main}>{children}</main>
     </div>
   );
 }
