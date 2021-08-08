66394c5edaa303aff133b70d94ed3b8e4bdfabd4
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-08-08 23:56:34 +0500

    setting up mongodb and connecting it to the application

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,25 @@
 // url is /new-new-meetup
 
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    MongoClient.connect(
+      'mongodb+srv://meetup-app:s12345@cluster0.16oys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne({ data });
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'successfully done insertion' });
   }
 }
 
